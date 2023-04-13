3728654bd97764a2d98a64efe6927e5c4e142465
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-04-13 20:50:22 +0530

    Added states for tasks and timings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-
-import './App.css';
-import Header from './components/Header';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-import AddIcon from '@mui/icons-material/Add';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import AddIcon from "@mui/icons-material/Add";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [timing, setTiming] = useState("");
+  const [data, setData] = useState([]);
+
   return (
     <div className="App">
       <Header />
-      <div className='form'>
-      <Stack direction="row" alignItems="center" spacing={2}>
-      <TextField id="outlined-basic" label="Name" variant="outlined" />
-      <TextField id="outlined-basic" label="Email" variant="outlined" />
-      <Button id="add" variant="contained" color="success">
-        <AddIcon />
-      </Button></Stack>
+      <div className="form">
+        <Stack direction="row" alignItems="center" spacing={2}>
+          <TextField
+            value={task}
+            onChange={(event) => setTask(event.target.value)}
+            id="outlined-basic"
+            label="Task"
+            variant="outlined"
+          />
+          <TextField
+            value={timing}
+            onChange={(event) => setTiming(event.target.value)}
+            id="outlined-basic"
+            label="Timing"
+            variant="outlined"
+          />
+          <Button id="add" variant="contained" color="success">
+            <AddIcon />
+          </Button>
+        </Stack>
       </div>
     </div>
   );
-}
+  }
 
 export default App;
