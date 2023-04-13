8454ddc1543e045d823d2ea9ff48b803c0f0aebb
Eshwar Nagaraj <eshwarnagaraj8099@gmail.com>
2023-04-13 23:16:06 +0530

    Added Remove button and styled it

--- src/components/Fields.js
+++ src/components/Fields.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const Fields = ({task, timing, index}) => {
   return (
     <div className="data_val">
           <h4>{task}</h4>
           <h4>{timing}</h4>
-          <h4>Remove</h4>
+          <Stack>
+          <Button variant="contained" color="error">
+                <DeleteIcon />
+        </Button>
+        </Stack>
     </div>
   )
 }
